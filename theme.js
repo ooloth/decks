8ea2587ee05faa62ec3de1123cc79e1572c6da13
Michael Uloth <michael.uloth@ecobee.com>
2019-09-02 20:11:44 -0400

    Initial commit

---
+++ theme.js
@@ -0,0 +1,74 @@
+export default {
+  colors: {
+    text: '#fff',
+    background: '#000',
+    primary: '#08f',
+    secondary: '#f0f',
+    darkgray: '#333'
+  },
+  styles: {
+    Slide: {
+      padding: `1rem 1rem`,
+      maxWidth: `100%`
+    },
+    fonts: {
+      monospace: 'Dank Mono'
+    },
+    h1: {
+      marginBottom: 0
+    },
+    h2: {
+      marginBottom: 0
+    },
+    h3: {
+      marginBottom: 0
+    },
+    p: {
+      marginBottom: 0,
+      maxWidth: `45ch`,
+      lineHeight: 1.3,
+      code: {
+        borderRadius: `.5rem`,
+        bg: 'secondary',
+        padding: `.05rem .5rem`,
+        color: '#000'
+      }
+    },
+    ul: {
+      marginTop: `1em`,
+      maxWidth: `45ch`
+    },
+    ol: {
+      marginTop: `1em`,
+      maxWidth: `45ch`
+    },
+    li: {
+      marginTop: `1.5rem`,
+      code: {
+        borderRadius: `.5rem`,
+        bg: 'secondary',
+        padding: `.05rem .5rem`,
+        lineHeight: 1.3,
+        color: '#000'
+      }
+    },
+    a: {
+      marginBottom: 0
+    },
+    pre: {
+      marginBottom: 0,
+      borderRadius: `.5rem`,
+      bg: 'secondary',
+      code: {
+        color: '#000'
+      }
+    },
+    blockquote: {
+      marginTop: 0,
+      marginBottom: 0,
+      maxWidth: `45ch`,
+      fontWeight: 400,
+      fontStyle: `italic`
+    }
+  }
+}
